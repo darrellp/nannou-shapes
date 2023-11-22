2c8fbe7d02898a140346ed7ad4a407b0efb0d3b7
Darrell Plank <jar_czar@msn.com>
2023-11-21 22:12:53 -0800

    Start of color ui

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,13 @@ fn main() {
 }
 
 mod draw {
+    use env_logger::fmt::Color;
     use log::LevelFilter;
+    use nannou::image::pnm::ArbitraryTuplType::RGB;
     use nannou::prelude::*;
     use crate::my_random::MyRandom;
     use nannou_egui::{self, egui, Egui};
+    use nannou_egui::egui::Color32;
 
     pub fn main() {
         env_logger::builder()
@@ -32,7 +35,10 @@ mod draw {
         max_scale: f32,
         pct_circles: f32,
         handle_mouse: bool,
+        alpha: u8,
         rng_seed: u64,
+        circle_color: [u8; 3],
+        square_color: [u8; 3],
     }
 
     impl Default for Settings {
@@ -44,6 +50,10 @@ mod draw {
                 max_scale: 1.0,
                 pct_circles: 0.5,
                 handle_mouse: true,
+                alpha: 255u8,
+                circle_color: [255_u8, 0_u8, 0_u8],
+                square_color: [0_u8, 0_u8, 255_u8],
+
                 rng_seed: MyRandom::from_range(1u64,u64::MAX) as u64,
             }
         }
@@ -95,7 +105,7 @@ mod draw {
         settings.rng_seed = app.elapsed_frames();
     }
 
-    fn update(app: &App, model: &mut Model, update: Update) {
+    fn update(_app: &App, model: &mut Model, update: Update) {
         let egui = &mut model.egui;
         let settings = &mut model.settings;
 
@@ -110,6 +120,11 @@ mod draw {
         }
 
         egui::Window::new("Shapes Settings").show(&ctx, |ui| {
+            egui::CollapsingHeader::new("Colors")
+                .show(ui, |ui| {
+                    ui.color_edit_button_srgb(&mut settings.square_color);
+                    ui.color_edit_button_srgb(&mut settings.circle_color);
+                });
             ui.add(egui::Slider::new(&mut settings.grid_count_x, 1..=100)
                 .text("X Count"));
             ui.add(egui::Slider::new(&mut settings.grid_count_y, 1..=100)
@@ -118,6 +133,10 @@ mod draw {
                 .text("Base Size"));
             ui.add(egui::Slider::new(&mut settings.max_scale, 1.0..=5.0)
                 .text("Max Scale"));
+            ui.add(egui::Slider::new(&mut settings.pct_circles, 0.0..=1.0)
+                .text("Circles"));
+            ui.add(egui::Slider::new(&mut settings.alpha, 0u8..=255u8)
+                .text("Alpha"));
         });
     }
 
@@ -126,10 +145,10 @@ mod draw {
     }
 
     fn view(app: &App, model: &Model, frame: Frame) {
-        let shapes_info = &model.settings;
+        let settings = &model.settings;
 
         // For consistent results from frame to frame
-        MyRandom::seed_from_u64(shapes_info.rng_seed);
+        MyRandom::seed_from_u64(settings.rng_seed);
 
         // Prepare to draw.
         let draw = app.draw();
@@ -138,18 +157,18 @@ mod draw {
 
         // Get boundary of the window to size everything correctly
         let boundary = app.window_rect();
-        let cell_width: f32 = (boundary.right() - boundary.left()) / shapes_info.grid_count_x as f32;
-        let cell_height: f32 = (boundary.top() - boundary.bottom()) / shapes_info.grid_count_y as f32;
+        let cell_width: f32 = (boundary.right() - boundary.left()) / settings.grid_count_x as f32;
+        let cell_height: f32 = (boundary.top() - boundary.bottom()) / settings.grid_count_y as f32;
         let cell_size = f32::min(cell_width, cell_height);
-        let shape_size = cell_size * shapes_info.base_size;
+        let shape_size = cell_size * settings.base_size;
         let mut pt_center = boundary.bottom_left() + Point2::new(-cell_width / 2.0, -cell_height / 2.0);
 
         let mut positions: Vec<Point2> = Vec::with_capacity(
-            shapes_info.grid_count_x * shapes_info.grid_count_y);
-        for _ in 0..shapes_info.grid_count_x {
+            settings.grid_count_x * settings.grid_count_y);
+        for _ in 0..settings.grid_count_x {
             pt_center.x += cell_width;
             pt_center.y = boundary.bottom() - cell_height / 2.0;
-            for _ in 0..shapes_info.grid_count_y {
+            for _ in 0..settings.grid_count_y {
                 pt_center.y += cell_height;
                 positions.push(pt_center);
             }
@@ -160,15 +179,15 @@ mod draw {
             (positions[i], positions[swap_index]) = (positions[swap_index], positions[i]);
         }
         for pt_center in positions {
-            let scale = MyRandom::get_float() * (shapes_info.max_scale - 1.0) + 1.0;
+            let scale = MyRandom::get_float() * (settings.max_scale - 1.0) + 1.0;
             let cur_size = shape_size * scale;
-            if MyRandom::get_float() < shapes_info.pct_circles
+            if MyRandom::get_float() < settings.pct_circles
             {
-                draw_circle_from_size_ctr(&draw, pt_center, cur_size);
+                draw_circle_from_size_ctr(&draw, pt_center, cur_size, settings.circle_color, settings.alpha);
             }
             else
             {
-                draw_quad_from_size_ctr(&draw, pt_center, cur_size);
+                draw_quad_from_size_ctr(&draw, pt_center, cur_size, settings.square_color, settings.alpha);
             }
         }
 
@@ -177,19 +196,21 @@ mod draw {
             model.egui.draw_to_frame(&frame).unwrap();}
     }
 
-    fn draw_quad_from_size_ctr(draw: &Draw, center: Point2, size: f32) {
+    fn draw_quad_from_size_ctr(draw: &Draw, center: Point2, size: f32, clr: [u8; 3], alpha: u8) {
         let size_d2 = size / 2.0;
         let v1 = center + Point2::new(size_d2, size_d2);
         let v2 = center + Point2::new(size_d2, -size_d2);
         let v3 = center + Point2::new(-size_d2, -size_d2);
         let v4 = center + Point2::new(-size_d2, size_d2);
+        let (r, g, b) = clr.into();
         draw.quad()
-            .color(BLUE)
+            .rgba8(r, g, b, alpha)
             .points(v1, v2, v3, v4);
     }
-    fn draw_circle_from_size_ctr(draw: &Draw, center: Point2, size: f32) {
+    fn draw_circle_from_size_ctr(draw: &Draw, center: Point2, size: f32, clr: [u8; 3], alpha: u8) {
+        let (r, g, b) = clr.into();
         draw.ellipse()
-            .color(RED)
+            .rgba8(r, g, b, alpha)
             .w(size)
             .h(size)
             .xy(center);
